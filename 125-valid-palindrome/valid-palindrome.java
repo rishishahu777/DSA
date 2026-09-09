287863443ff1f274052dafc7d30bdc776fef7573
Rishi Shahu <rishishahu11@gmail.com>
2026-09-09 20:28:22 +0530

    Time: 14 ms (28.39%) | Memory: 46.5 MB (19.07%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,10 +2,8 @@ class Solution {
     public boolean isPalindrome(String s) {
         String n = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         char[] arr = n.toCharArray();
-
         int left = 0;
         int right = arr.length-1;
-
         while(left<right){
             if(arr[left]!=arr[right]){
                 return false;
@@ -14,6 +12,7 @@ class Solution {
             right--;
         }
         return true;
+
 
     }
 }
